bc9e7ca9a755263904149c662a5e99ad0f4dfb3c
Zulaicho <101866835+Zulaicho@users.noreply.github.com>
2022-07-12 12:42:52 +0700

    Add files via upload

---
+++ Penjualan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi
+{
+     
+    
+        internal class Penjualan
+        {
+            public Penjualan()
+            {
+            }
+
+            public int Code { get; set; }
+            public string Jenis { get; set; }
+            public string Nama { get; set; }
+            public string Tanggal { get; set; }
+            public double Total { get; set; }
+        }
+    
+}
